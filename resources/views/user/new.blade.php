c4ca75d59bf02f3c611bad0856183000a1abdf15
lagoma619 <lagoma619@hotmail.com>
2022-07-10 03:36:52 -0500

    NEW BLADE

--- resources/views/user/new.blade.php
+++ resources/views/user/new.blade.php
@@ -3,9 +3,10 @@
 @section('scripts')
     <!-- SELECTOR DE FECHA -->
     <script src="{{asset('assets/vendor/jquery/dist/jquery.min.js')}}"></script>
-    <link rel="stylesheet" href="{{asset('assets/vendor/bootstrap/datepicker/css/bootstrap-datepicker3.css')}}">
-    <script src="{{asset('assets/vendor/bootstrap/datepicker/js/bootstrap-datepicker.js')}}"></script>
-    <script src="{{asset('assets/vendor/bootstrap/datepicker/locales/bootstrap-datepicker.es.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('assets/vendor/datepicker/css/bootstrap-datepicker3.css')}}">
+    <script src="{{asset('assets/vendor/datepicker/js/bootstrap-datepicker.js')}}"></script>
+    <script src="{{asset('assets/vendor/datepicker/js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/datepicker/locales/bootstrap-datepicker.es.min.js')}}"></script>
     <script>
         $('.datepicker').datepicker({
             format: "yyyy-mm-dd",
@@ -23,9 +24,29 @@
 
     <!-- Card Basic Info -->
     <div class="card mt-4" id="basic-info">
+        <div class="card-body">
         <div class="card-header">
-            <h5>DATOS DEL USUARIO</h5>
+            <div class="row">
+                <div class="col-6">
+                    <div class="form-group">
+                        <h5>DATOS DEL USUARIO</h5>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <label class="form-check-label mb-0">
+                        <label id="activo">
+                            ACTIVO
+                        </label>
+                    </label>
+                    <div class="form-check form-switch ms-2">
+                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault23" checked onchange="visible()">
+                    </div>
+                </div>
+            </div>
         </div>
+
+        </div>
+
         <div class="card-body pt-0">
             <div class="row">
                 <div class="col-6">
@@ -112,6 +133,20 @@
                         <label class="form-label mt-4">FECHA DE CONTRATACIÓN</label>
                         <div class="input-group">
                             <input class="form-control datepicker" type="text"  name="fechacontrato" placeholder="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label mt-4">CONTRASEÑA</label>
+                    <input class="form-control" id="contrasena" type="password" placeholder="******" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+                        <label class="form-label mt-4">TIPO DE USUARIO</label>
+                        <div class="input-group">
+                            <input id="location" name="city" class="form-control" type="text" placeholder="MENSAJERO" onkeyup="this.value = this.value.toUpperCase();">
                         </div>
                     </div>
                 </div>
