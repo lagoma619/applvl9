3375a91def85856ea8061345322576816036b6bc
lagoma619 <lagoma619@hotmail.com>
2022-11-03 15:39:27 -0500

    20221103

--- app/Http/Controllers/Api/DomiciliosController.php
+++ app/Http/Controllers/Api/DomiciliosController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cliente;
 use App\Models\Domicilio;
 use App\Models\TipoServicio;
+use App\Models\User;
 use App\Models\Vehiculo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DomiciliosController extends Controller
 {
@@ -52,6 +55,63 @@ class DomiciliosController extends Controller
     public function tiposVehiculos()
     {
         return Vehiculo::all();
+    }
+
+    public function nuevoDomicilio(Request $request)
+    {
+        $origen1 = $request->input('domicilio_origen1');
+        $origen2 = $request->input('domicilio_origen2');
+        $destino1 = $request->input('domicilio_destino1');
+        $destino2 = $request->input('domicilio_destino2');
+
+        //ESTABLECE ORIGEN
+        if (!empty($origen1) || !empty($origen2)){
+            if ($origen2){
+                $request['domicilio_origen'] = $origen2;
+            } elseif ($origen1){
+                $request['domicilio_origen'] = $origen1;
+            }
+        }
+
+        //ESTABLECE DESTINO
+        if (!empty($destino1) || !empty($destino2)){
+            if ($destino2){
+                $request['domicilio_destino'] = $destino2;
+                ;
+            } elseif ($destino1){
+                $request['domicilio_destino'] = $destino1;
+            }
+        }
+        //ESTADO DOMICILIO: SIN ASIGNAR
+
+
+        $asignado = $request->input('domicilio_asignado_a');
+        if (!empty($asignado)){
+            $domicilio['domicilio_id_estado_domicilio'] = 2; //EN CURSO
+        } else {
+            $domicilio['domicilio_id_estado_domicilio'] = 1; //SIN ASIGNAR
+        }
+
+
+        //ESTABLECE CLIENTE QUE SOLICITA DOMICILIO
+        $personaactual = User::join('personas', 'persona_id','=', 'id_persona')->where('persona_id',auth()->id())->get('persona_id_cliente');
+        $clientesolicita = Cliente::all()->where('cliente_id','=',$personaactual[0]->persona_id_cliente);
+        $request['domicilio_id_cliente'] = $clientesolicita[1]->cliente_id;
+
+        //dd($request['domicilio_id_cliente']);
+
+
+        //dd($request);
+        DB::transaction(function () use ($request){
+            $domicilio = Domicilio::create($request->all());
+        });
+        $notification = 'El domicilio se ha creado correctamente.';
+
+        return redirect(route('domicilios.index'))->with(compact('notification'));
+
+
+
+        dd($request);
     }
 
 }
